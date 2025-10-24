3ef0f213f67cfcbd55fde42a9b1df0f6537c9b62
Zain676 <mzainq9@gmail.com>
2025-10-24 18:33:39 +0500

    completed

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-
 const burger = document.getElementById("burger");
 const overlay = document.getElementById("overlay");
+let navbarNav = document.getElementById("nav-links");
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     overlay.classList.toggle("active");
+    navbarNav.style.display = "none";
+
 });
