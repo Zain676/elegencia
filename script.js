a2dad99567de6f75d1958f75074f740f1921b52a
Zain676 <mzainq9@gmail.com>
2025-10-24 19:48:22 +0500

    completed

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const burger = document.getElementById("burger");
 const overlay = document.getElementById("overlay");
 let navbarNav = document.getElementById("nav-links");
 
+
+
+window.addEventListener('scroll', () => {
+    const navbars = document.querySelectorAll(".navbar,#nav-links");
+    if (window.scrollY > 50) {
+        navbars.forEach(nav => nav.classList.add('scrolled'));
+    } else {
+        navbars.forEach(nav => nav.classList.remove('scrolled'));
+    }
+});
+
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     overlay.classList.toggle("active");
